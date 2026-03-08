72db1866875d8cd44284091d32c63c1169bddcce
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2026-03-08 16:00:50 +0600

    [lucru] (feat/app-structure): Enhance app structure with TopHeader and MainHeader components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import TopHeader from "@/components/TopHeader";
+import MainHeader from "@/components/MainHeader";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${montserrat.variable} font-sans`}>
+      <TopHeader />
+      <MainHeader />
+      <Component {...pageProps} />
+    </div>
+  );
 }
